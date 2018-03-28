35b8af1257e9956454e25819d9a702d8c75d6cec
Matthew DEVILLE <mdeville@e2r7p21.42.fr>
2018-03-28 22:47:59 +0200

    Final fixes

--- ex11/do_op_2.php
+++ ex11/do_op_2.php
@@ -1,31 +1,45 @@
-#! /usr/bin/php
-<?php
-
-if ($argc != 2)
-	exit("Incorrect Parameters\n");
-
-$tmp = preg_split("#([ \t\+\-\*%/])#", $argv[1], -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
-$res = array();
-foreach ($tmp as $val) {
-	if ($val != " " && $val != "\t")
-		array_push($res, $val);
-}
-if (count($res) != 3 || !is_numeric($res[0]) || !is_numeric($res[2]))
-	exit("Syntax Error\n");
-$a = intval($res[0]);
-$b = intval($res[2]);
-if ($res[1] == "+")
-	echo $a + $b;
-else if ($res[1] == "-")
-	echo $a - $b;
-else if ($res[1] == "*")
-	echo $a * $b;
-else if ($res[1] == "/" && $b)
-	echo $a / $b;
-else if ($res[1] == "%" && $b)
-	echo $a % $b;
-else
-	exit("Syntax Error\n");
-echo "\n";
-
+#!/usr/bin/php
+<?PHP
+    if ($argc != 2)
+        exit("Incorrect Parameters\n");
+    function ft_output($item)
+    {
+        if (!is_numeric($item[0]) || !is_numeric($item[2]))
+            exit("Syntax Error\n");
+        if ($item[1] == "+")
+            echo ($item[0] + $item[2])."\n";
+        else if ($item[1] == "-")
+            echo ($item[0] - $item[2])."\n";
+        else if ($item[1] == "*")
+            echo ($item[0] * $item[2])."\n";
+        else if ($item[1] == "/" && $item[2] != 0)
+            echo ($item[0] / $item[2])."\n";
+        else if ($item[1] == "%" && $item[2] != 0)
+            echo ($item[0] % $item[2])."\n";
+        else
+            exit("Syntax Error\n");
+    }
+    $item = preg_split("#[ \t]|([\+\-\*\/\%])#", $argv[1], -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+    $new = array();
+    $max = count($item);
+    for ($i = 0; $i < $max; $i++)
+    {
+        if ($item[$i] === "-" && $i + 1 < $max && is_numeric($item[$i + 1]))
+        {
+            $new[] = $item[$i].$item[$i + 1];
+            $i++;
+        }
+        else
+            $new[] = $item[$i];
+    }
+    $tmp = $new;
+    if (count($new) == 2 && is_numeric($new[0]) && is_numeric($new[1]) && $new[0] <= 0 && $new[1] <= 0)
+    {
+        $new[1] = "+";
+        $new[2] = $tmp[1];
+    }
+    if (count($new) != 3)
+        exit("Syntax Error\n");
+    ft_output($new);
+    return ;
 ?>
